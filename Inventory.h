77496574bcd85bd267bcf2f4abee20c3b73e7ece
mshen010 <mshen010@ucr.edu>
2015-01-03 16:46:51 -0800

    Player Inventory

---
+++ Inventory.h
@@ -0,0 +1,14 @@
+#ifndef inventory_H
+#define inventory_H
+
+using namespace std;
+
+//Not utilized in prototype
+class Inventory
+{
+    public:
+        void PrintInventory(vector<string> &items)
+    private:
+};
+
+#endif
