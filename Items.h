861b575cf33331f685ddf02fa195489246f62da6
mshen010 <mshen010@ucr.edu>
2015-01-03 16:46:17 -0800

    Items

---
+++ Items.h
@@ -0,0 +1,25 @@
+#ifndef items_H
+#define items_H
+
+using namespace std;
+
+
+class Item
+{
+    public:
+        //ISSUE: What function?
+    private:
+        int attack;
+        int defense;
+        int value;
+};
+
+// **Will not utilized within Prototype 1.0
+    // class Potion
+    // {
+    //     public:
+    //     private:
+    // };
+    
+    
+#endif
