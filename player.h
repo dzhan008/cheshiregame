92f16df1507e246028e0e80d0756973e6964e108
JustSparky <atran049@ucr.edu>
2015-01-08 14:37:26 -0800

    Added a few functions

--- player.h
+++ player.h
@@ -1,5 +1,6 @@
 #ifndef PLAYER_H
 #define PLAYER_H
+
 #include <vector>
 #include <string>
 
@@ -8,34 +9,39 @@ using namespace std;
 class player
 {
     public:
+    
+        //Constructor
+        player();
+        player(string pName, int health, int lvl, int amount);
 
-    /*Get Functions: Returns the respective value.*/
-    int getHP();
-    int getlevel();
-    int getmoney();
-    int getexp();
-
-    string getname();
-
-    /*Set Functions: Sets every value into a certain number.*/
-    void setHP(int& health); //Changes currHP to the updated hp value after a certain instance.
-    void setlevel(int&level);
-    void setmoney(int& money);
-    void setexp(int& exp);
-    void setstats(vector<int>& stats);
-
-    /*Misc*/
-    void displayInfo();
+        /*Get Functions: Returns the respective value.*/
+        int getMaxHP();
+        int getHP();
+        int getlevel();
+        int getmoney();
+        int getexp();
+    
+        string getname();
+    
+        /*Set Functions: Sets every value into a certain number.*/
+        void setHP(int health); //Changes currHP to the updated hp value after a certain instance.
+        void setlevel(int&level);
+        void setmoney(int& money);
+        void setexp(int expx1);
+        void setstats(vector<int>& stats);
+    
+        /*Misc*/
+        void displayInfo();
 
     private:
-    int maxHP; //The maximum health points of the player.
-    int currHP; //The current health the player has at the moment.
-    int plevel; //The player's current level.
-    int pmoney; //The amount of money the player has.
-    int pexp; //The amount of exp the player has.
-    //int maxexp; //The maximum amount of exp the player has. May be put into another class.
-    string playername; //The player's name.
-    vector<int> playerstats; //Current stats for the player.
+        int maxHP; //The maximum health points of the player.
+        int currHP; //The current health the player has at the moment.
+        int plevel; //The player's current level.
+        int pmoney; //The amount of money the player has.
+        int pexp; //The amount of exp the player has.
+        //int maxexp; //The maximum amount of exp the player has. May be put into another class.
+        string playername; //The player's name.
+        vector<int> playerstats; //Current stats for the player.
 
 
 };
