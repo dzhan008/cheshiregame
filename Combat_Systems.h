6c652e90cc88a1ce9399534be3ee6f01fca7cc27
JustSparky <atran049@ucr.edu>
2015-01-08 14:35:33 -0800

    Completed!

--- Combat_Systems.h
+++ Combat_Systems.h
@@ -1,27 +1,28 @@
-//Mostly finished, just need to meet up and clean up the program.
-
 #ifndef _COMBAT_SYSTEM_H_
 #define _COMBAT_SYSTEM_H_
 
 using namespace std;
 
+class entity;
+class player;
+
 class Combat_System{
     private:
         //0 For player turn, 1 for enemy turn
         int turn;
-        vector<entity> enemies;
-        entity enemy;
-        player play;
+        //vector<entity> enemies;
+        entity* enemy;
+        player* play;
     public:
         Combat_System();
-        Combat_System(Player p);
-        void runBattle(vector<entity> enemy);
-        void runBattle(entity enemy);
-        void updatePlayer(Player p);
+        Combat_System(player* p);
+        //void runBattle(vector<entity> enemy);
+        void runBattle(entity* enemy);
+        void updatePlayer(player* p);
     private:
         int randNumber();
         int promptChoices();
-        int promptEnemyChoice();
+        //int promptEnemyChoice();
         double calculateAccuracy();
         int calculateDamage(bool defend);
         int calculateTurn();
