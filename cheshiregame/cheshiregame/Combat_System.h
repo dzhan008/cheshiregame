d081e9947e3157e80280e884a63e96c488e0f298
JustSparky <atran049@ucr.edu>
2015-03-06 22:37:50 -0800

    Multi Battle

--- cheshiregame/cheshiregame/Combat_System.h
+++ cheshiregame/cheshiregame/Combat_System.h
@@ -11,23 +11,21 @@ class Combat_System{
     private:
         //0 For player turn, 1 for enemy turn
         int turn;
-        //vector<entity> enemies;
-        entity* enemy;
         player* play;
     public:
         Combat_System();
         Combat_System(player* p);
-        //void runBattle(vector<entity> enemy);
+        void runBattle(vector<entity*> enemy);
         void runBattle(entity* enemy);
         void updatePlayer(player* p);
     private:
         int randNumber();
         int promptChoices();
-        //int promptEnemyChoice();
+        int promptEnemyChoice(vector<entity*> enemy);
         double calculateAccuracy();
         int calculateDamage(bool defend);
         int calculateTurn();
-        bool calculateEnemyChoice();
+        bool calculateEnemyChoice(entity* enemy);
 };
 
 #endif
