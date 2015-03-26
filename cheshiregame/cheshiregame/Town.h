5a0ec375bb9b627b6943db15c9fbd72e78adffef
K K <kkita004@ucr.edu>
2015-03-25 23:52:55 -0700

    Updated Town.h

--- cheshiregame/cheshiregame/Town.h
+++ cheshiregame/cheshiregame/Town.h
@@ -3,13 +3,34 @@
 
 #include <string>
 #include <vector>
+#include <utility>
 
 class Town {
 private:
-    std::string name;
-    std::vector<std::string> options;
-public: 
-    std::vector<std::string> get_options();
+    std::string townname;
+    /* Store goes here */
+    Store s;
+    /* Blacksmith goes here */
+    // BlackSmith bs;
+    /* Tavern goes here */
+    // Tavern t;
+    /* Townspeoples, may put them in a tavern or something*/
+    /* Might want to create a person class to interact with for quests and such*/
+    std::vector<std::pair<std::string, std::string> > people;
+
+    /* Loads town from text file*/
+    void fill_town(const std::string &text_file);
+
+public:
+    Town();
+    Town(std::string filename);
+    void run();
+    void inn();
+    void tavern();
+    void blacksmith();
+    void store();
+    void talk();
+
 };
 
 #endif
