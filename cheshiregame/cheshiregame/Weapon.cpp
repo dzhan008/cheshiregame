191780de5b2e3f1d0cb9b590a22a56b93ccba616
mshen010 <mshen010@ucr.edu>
2015-03-27 16:20:18 -0700

    Create Weapon.cpp

---
+++ cheshiregame/cheshiregame/Weapon.cpp
@@ -0,0 +1,19 @@
+#include "Weapon.h"
+
+#include <iostream>
+
+using namespace std;
+
+bool Weapon::equipWeapon(string item, player p)
+{
+	for(int i = 0; i < p.getInvSize; i++)
+	{
+		if(p.inventory_search(item))
+		{
+			p.equip_slot(6, Item(item));
+			return true;
+		}
+	}
+	cout << "Item not found." << endl;
+	return false;
+}
