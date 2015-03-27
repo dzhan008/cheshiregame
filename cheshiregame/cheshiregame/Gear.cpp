73319a1a202cd6fcd1ce9a6300d389ed99cc6be0
mshen010 <mshen010@ucr.edu>
2015-03-27 16:28:30 -0700

    Create Gear.cpp

---
+++ cheshiregame/cheshiregame/Gear.cpp
@@ -0,0 +1,25 @@
+#include "Gear.h"
+
+#include <iostream>
+
+using namespace std;
+
+bool Gear::equipGear(string item, unsigned bodyPart, player p)
+{
+	if (bodyPart > 6)
+	{
+		cout << "Invalid location." << endl;
+		return false;
+	}
+	for (int i = 0; i < p.getInvSize; i++)
+	{
+		if (p.inventory_search(item))
+		{
+			//Check bodyPart is a valid num, where each gear should go
+			p.equip_slot(bodyPart, Item(item));
+			return true;
+		}
+	}
+	cout << "Item not found." << endl;
+	return false;
+}
