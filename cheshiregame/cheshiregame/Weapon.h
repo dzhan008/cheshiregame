8b8250b82257b3cddec815b9b4fd23b691aa3867
mshen010 <mshen010@ucr.edu>
2015-03-27 16:29:15 -0700

    Create Weapon.h

---
+++ cheshiregame/cheshiregame/Weapon.h
@@ -0,0 +1,15 @@
+#ifndef WEAPON_H
+#define WEAPON_H
+
+#include "Item.h"
+#include "Player.h"
+
+class Weapon : public Item
+{
+	private:
+		int damageBoost;
+	public:
+		bool equipWeapon(string itemName, player p);
+};
+
+#endif
