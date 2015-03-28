cd65fbbf02a22d73b19cc330307924a4a0702d9a
danyo3 <ddiep003@ucr.edu>
2015-03-27 18:22:52 -0700

    Map movement and display functioning properly

--- cheshiregame/cheshiregame/map.cpp
+++ cheshiregame/cheshiregame/map.cpp
@@ -9,12 +9,14 @@ using namespace std;
 //aka open space
 map::map()
 {
-	mapitems.resize(100);
+	for (int i = 0; i < 10; ++i) {
+		mapitems.push_back(vector<int>(10));
+	}
 	//creates 10 * 10 map
-	//playerposition.resize(100);
-	playerposition.first = 0; // row
-	playerposition.second = 0;// column
-	mapitems.at(0).at(0) = 2;
+	playerposition.first = 5; // row
+	playerposition.second = 5;// column
+	mapitems.at(playerposition.first).at(playerposition.second) = 2;
+	//mapitems.at(1).at(0) = 1;
 	//keeps track of player position
 }
 map::map(int, string& file)
@@ -27,7 +29,9 @@ map::map(int, string& file)
 		std::cout << "Error mapfile not open. " << std::endl;
 	}
 	int blocks;
+	//blocks are the spaces that make up the map
 	vector<int> row;
+	//to push back a vector of vectors I cant push back one element but rather one vector.
 	while (mapfile >> blocks)
 	{
 		row.push_back(blocks);
@@ -45,8 +49,10 @@ void map::updateplayer()
 			{
 				int x = i;
 				int y = j;
+				//cout << "Player coordinates: " << playerposition.first << " " << playerposition.second << endl;
 				mapitems.at(playerposition.first).at(playerposition.second) = 2;
 				mapitems.at(x).at(y) = 0;
+				return;
 			}
 		}
 		
@@ -54,8 +60,12 @@ void map::updateplayer()
 }
 void map::moveLeft()
 {
-	//may go out of bounds
-		if (wallcheck(playerposition.first, playerposition.second - 1) == false)
+	//Redundancy check so that wallcheck function doesnt go out of bounds.
+	if (playerposition.second == 0)
+	{
+		return;
+	}
+	if (wallcheck(playerposition.first, playerposition.second - 1) == false)
 		{
 			if (playerposition.second == 0)
 			{
@@ -69,19 +79,34 @@ void map::moveLeft()
 }
 void map::moveRight()
 {
+	if (playerposition.second == mapitems.at(0).size() - 1)
+	{
+		return;
+	}
 	if (wallcheck(playerposition.first, playerposition.second + 1) == false)
 	{
-		if (playerposition.second == mapitems.at(0).size()-1)
+		//cout << "There is no wall. " << endl;
+		//cout << "Player coordinates: " << playerposition.first << " " << playerposition.second << endl;
+		//cout << "Edge: " << mapitems.at(0).size() - 1 << endl;
+		if (playerposition.second == mapitems.at(0).size() - 1)
 		{
 			return;
 		}
-		playerposition.second++;
-		updateplayer();
+		else
+		{
+			playerposition.second++;
+			updateplayer();
+
+		}
 	}
 	return;
 }
 void map::moveUp()
 {
+	if (playerposition.first == 0)
+	{
+		return;
+	}
 	if (wallcheck(playerposition.first - 1, playerposition.second) == false)
 	{
 		if (playerposition.first == 0)
@@ -95,8 +120,13 @@ void map::moveUp()
 }
 void map::moveDown()
 {
+	if (playerposition.first == mapitems.at(mapitems.size() - 1).size() - 1)
+	{
+		return;
+	}
 	if (wallcheck(playerposition.first+1, playerposition.second) == false)
 	{
+		//cout << "Moving down:" << endl;
 		if (playerposition.first == mapitems.at(mapitems.size()-1).size() -1)
 		{
 			return;
@@ -124,7 +154,7 @@ void map::display()
 {
 	for (int i = 0; i < mapitems.size(); i++)
 	{
-		for (int j = 0; j < mapitems.size(); j++)
+		for (int j = 0; j < mapitems.at(i).size(); j++)
 		{
 			cout << mapitems.at(i).at(j) << " ";
 		}
