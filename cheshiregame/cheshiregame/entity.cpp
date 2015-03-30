e905d309e5671efb6c60a8c654c11663efcb1d86
JustSparky <atran049@ucr.edu>
2015-03-29 17:56:19 -0700

    Update entity.cpp

--- cheshiregame/cheshiregame/entity.cpp
+++ cheshiregame/cheshiregame/entity.cpp
@@ -9,10 +9,10 @@ using namespace std;
 /*Create Entity*/
 
 Entity::Entity()
-: name("Enemy"), maxHP(10), currHP(10), level(1), numEXP(1), min_dmg(1), max_dmg(10), def(0)
+: name("Enemy"), maxHP(10), currHP(10), level(1), numEXP(1), min_dmg(1), max_dmg(10), def(0), defending(false)
 { }
 Entity::Entity(string entityName, int health, int lvl, int amount, int d)
-: name(entityName), maxHP(health), currHP(health), level(lvl), numEXP(amount), min_dmg(1), max_dmg(10), def(d)
+: name(entityName), maxHP(health), currHP(health), level(lvl), numEXP(amount), min_dmg(1), max_dmg(10), def(d), defending(false)
 { }
 
 
@@ -72,6 +72,9 @@ int Entity::calculateDamage(Ally ally, bool defend){
 	return trueDamage;
 }
 
+bool Entity::isDefending(){
+	return defending;
+}
 /*Stats*/
 //Sets the stats of the entity
 void Entity::setEntityStats(int str, int agi, int vit, int dex, int luk)
