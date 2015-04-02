7e4dde2187df737d353841b752adcce29dfc2b52
JustSparky <atran049@ucr.edu>
2015-04-01 23:22:03 -0700

    Update Combat_System.h

--- cheshiregame/cheshiregame/Combat_System.h
+++ cheshiregame/cheshiregame/Combat_System.h
@@ -5,28 +5,28 @@
 using namespace std;
 
 class Combat_System{
-    private:
-        //0 For player turn, 1 for enemy turn
-        int turn;
-        player* play;
+	private:
+		//0 For player turn, 1 for enemy turn
+		int turn;
+		player* play;
 		vector<Ally*> allies;
-    public:
-        Combat_System();
-        Combat_System(player* p);
-		Combat_System(player* p, vector<Ally*> allies);
-        void runBattle(vector<Entity*> enemy);
-        void runBattle(Entity* enemy);
-        void updatePlayer(player* p);
-        void updateAllies(vector<Ally*> ally);
-    private:
-        int randNumber();
-        int promptChoices();
-        int promptEnemyChoice(vector<Entity*> enemy);
-        double calculateAccuracy();
-        int calculateDamage(bool defend);
-        int calculateTurn();
-        int attackChoice(int size);
-        bool calculateEnemyChoice(Entity* enemy);
+	public:
+		Combat_System();
+		Combat_System(player& p);
+		Combat_System(player& p, vector<Ally> allies);
+		void runBattle(vector<Entity*> enemy);
+		void runBattle(Entity* enemy);
+		void updatePlayer(player* p);
+		void updateAllies(vector<Ally*> ally);
+        private:
+		int randNumber();
+		int promptChoices();
+		int promptEnemyChoice(vector<Entity*> enemy);
+		double calculateAccuracy();
+		int calculateDamage(bool defend);
+		int calculateTurn();
+		int attackChoice(int size);
+		bool calculateEnemyChoice(Entity* enemy);
 		bool calculateAllyChoice(Ally* ally);
 };
 
